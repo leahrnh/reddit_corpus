40217ca667d7dab271ed39b7569b6b78c1995048
Leah <eahlay@gmail.com>
2015-01-29 14:10:09 -0500

    gets comment pairs from hot topics in specified subreddit

--- reddit_corpus.py
+++ reddit_corpus.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import praw
 import json
 import sys
@@ -8,8 +9,6 @@ if len(sys.argv) < 3:
 
 subred = sys.argv[1]
 output = sys.argv[2]
-
-
 
 user_agent = ("Corpus creator by Leah https://github.com/leahrnh")
 r = praw.Reddit(user_agent=user_agent)
@@ -22,16 +21,17 @@ def readcomments(q, comments):
       for comment in comments:
         global comment_pairs
         comment_pairs += 1
+        print("Found comment pair: " + comment.body[:20] + "...")
         f.write(json.dumps({"question":q, "answer":comment.body}, sort_keys=False, indent=4))
         readcomments(comment.body, comment.replies)
 
 subreddit = r.get_subreddit(subred)
-#for submission in subreddit.get_hot(limit=1):
-submission = r.get_submission(submission_id='2u0wiq')
-q = submission.title
-submission.replace_more_comments(limit=None, threshold=0)
-forest_comments = submission.comments
-readcomments(q, forest_comments)
+for submission in subreddit.get_hot(limit=10):
+    #submission = r.get_submission(submission_id='2u0wiq')
+    q = submission.title
+    submission.replace_more_comments(limit=None, threshold=0)
+    forest_comments = submission.comments
+    readcomments(q, forest_comments)
 
-print "Created file with %d comment pairs" % comment_pairs
+print("Created file with %d comment pairs" % comment_pairs)
 f.close()
